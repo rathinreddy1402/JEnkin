d8cd5b02730778e4fc32027e4c26f44cbf0a5b05
rathinreddy1402 <rathinreddy1402@gmail.com>
2023-07-13 11:12:53 +0530

    push

---
+++ JenkinsExecuition/src/test/java/Demo/SampleTest.java
@@ -0,0 +1,14 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+	@Test
+	public void hiTest()
+	{
+		System.out.println("hi");
+		System.out.println("gud mrng");
+	}
+	
+}
